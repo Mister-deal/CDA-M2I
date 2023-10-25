c87f4e48a400819c2dfbeb62ed498a21706c55ff
Maxime Morvan <max-94400@hotmail.fr>
2023-10-25 17:31:55 +0200

    ajout product.js

--- nodeBoutique/routes/products.js
+++ nodeBoutique/routes/products.js
@@ -1,7 +1,12 @@
 import express from 'express'
+import { Product } from '../classes/Product';
+import ProductDao from '../dao/ProductDao';
 
 const products = express.Router();
+products.use(express.json())
+const productDao = new ProductDao();
+productDao.readFile();
 
 products.get('/products', (req, res) =>{
-    res.send("liste des produits")
+    res.send(productDao.getAll())
 })
