6c185974d59408b38d1845cf095b4738f3d4984b
Maxime Morvan <max-94400@hotmail.fr>
2023-11-22 17:17:41 +0100

    ajout commentaires

--- Java/demo/src/main/java/org/example/exercices/poo_exercice_maison/Door.java
+++ Java/demo/src/main/java/org/example/exercices/poo_exercice_maison/Door.java
@@ -2,15 +2,16 @@ package org.example.exercices.poo_exercice_maison;
 
 public class Door {
     private String color;
-
+    //création d'un getter afin d'y inscrire les données de color
     public String getColor() {
         return color;
     }
 
+    //création d'un setter de couleur
     public void setColor(String color) {
         this.color = color;
     }
-
+    //création d'une méthode pouvant récupérer la couleur
     public void display() {
         System.out.println("Je suis une porte, ma couleur est " + color);
     }
