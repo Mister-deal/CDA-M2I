6b86910de6243aa5fe8738b5761cba85e185d69e
Maxime Morvan <max-94400@hotmail.fr>
2024-02-28 17:30:33 +0100

    ajout demo programmation

--- Spring/demos/demo_spring_reactive/src/main/java/com/example/demo_spring_reactive/model/Message.java
+++ Spring/demos/demo_spring_reactive/src/main/java/com/example/demo_spring_reactive/model/Message.java
@@ -2,13 +2,23 @@ package com.example.demo_spring_reactive.model;
 
 import lombok.Builder;
 import lombok.Data;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.relational.core.mapping.Column;
+import org.springframework.data.relational.core.mapping.Table;
 
 import java.time.LocalDateTime;
 
 @Data
 @Builder
+@Table("message")
 public class Message {
+
+    @Id
+    private Long id;
+
     private String sender;
     private String content;
+
+    @Column("message_date_time")
     private LocalDateTime messageDateTime;
 }
--- Spring/demos/demo_spring_reactive/src/main/java/com/example/demo_spring_reactive/service/MessageService.java
+++ Spring/demos/demo_spring_reactive/src/main/java/com/example/demo_spring_reactive/service/MessageService.java
@@ -1,8 +1,12 @@
 package com.example.demo_spring_reactive.service;
 
 import com.example.demo_spring_reactive.model.Message;
+import com.example.demo_spring_reactive.repository.MessageRepository;
+import io.r2dbc.spi.ConnectionFactory;
+import org.springframework.r2dbc.core.DatabaseClient;
 import org.springframework.stereotype.Service;
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 import reactor.core.publisher.Sinks;
 
 import java.time.LocalDateTime;
@@ -11,16 +15,40 @@ import java.time.LocalDateTime;
 public class MessageService {
 
     private final Sinks.Many<Message> sink;
+    private final MessageRepository messageRepository;
+    private final ConnectionFactory connectionFactory;
 
-    public MessageService() {
+    public MessageService(MessageRepository messageRepository, ConnectionFactory connectionFactory) {
+        this.messageRepository = messageRepository;
+        this.connectionFactory = connectionFactory;
+        createTable();
         sink = Sinks.many().multicast().onBackpressureBuffer();
     }
 
     public void sendMessage(String sender, String content) {
-        sink.tryEmitNext(Message.builder().content(content).sender(sender).messageDateTime(LocalDateTime.now()).build());
+        Message message = Message.builder().content(content).sender(sender).messageDateTime(LocalDateTime.now()).build();
+        messageRepository.save(message).subscribe((m) ->sink.tryEmitNext(m));
     }
 
     public Flux<Message> getFlux() {
+        messageRepository.findAll().onBackpressureBuffer().subscribe((m)-> {
+            //Une action pour vérifier qu'un message n'est pas dans le sink déjà
+            System.out.println(m);
+            sink.tryEmitNext(m);
+        });
         return sink.asFlux();
+    }
+
+    private void createTable() {
+        DatabaseClient databaseClient = DatabaseClient.create(connectionFactory);
+        Mono<Void> monoCreateTable = databaseClient.sql("CREATE TABLE IF NOT EXISTS " +
+                "message(" +
+                "id int primary key auto_increment," +
+                        "content TEXT," +
+                        "sender varchar(100)," +
+                        "message_date_time datetime" +
+                ")")
+                .then();
+        monoCreateTable.subscribe();
     }
 }
